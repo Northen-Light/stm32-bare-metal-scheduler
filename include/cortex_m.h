71c3421bf58ea785d712938d9dbec44fb5276f19
Prateek Patnaik <prateekpatnaikcomp@gmail.com>
2026-07-30 17:50:03 +0530

    Add cortex_m bit field macros

--- include/cortex_m.h
+++ include/cortex_m.h
@@ -4,6 +4,11 @@
 #define SCB_ICSR_REG                  *((volatile uint32_t *) 0xE000ED04U)
 #define SCB_SHPR3_REG                 *((volatile uint32_t *) 0xE000ED20U)
 
+#define SCB_ICSR_PENDSV_BIT           (1U << 28U)
+
+#define SCB_SHPR_PENDSV_BIT_FIELD     (0xFFU << 16U)
+#define SCB_SHPR_SYSTICK_BIT_FIELD    (0xFEU << 24U)
+
 #define INITIAL_XPSR                  (1U << 24U)
 
 void cortex_m_start_first_task(void);
--- src/cortex_m.c
+++ src/cortex_m.c
@@ -9,11 +9,11 @@ void cortex_m_start_first_task(void) {
 }
 
 void cortex_m_set_pending_task_switch(void) {
-  SCB_ICSR_REG = (1U << 28U);
+  SCB_ICSR_REG = SCB_ICSR_PENDSV_BIT;
 }
 
 void cortex_m_exception_priority_init(void) {
-  SCB_SHPR3_REG = (0xFFU << 16U) | (0xFEU << 24U);
+  SCB_SHPR3_REG = SCB_SHPR_PENDSV_BIT_FIELD | SCB_SHPR_SYSTICK_BIT_FIELD;
 }
 
 __attribute((naked))
